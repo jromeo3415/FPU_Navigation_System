3459454df8771b7af42dcf539517c33b6580c164
jromeo3415 <jromeo3415@floridapoly.edu>
2025-03-08 14:58:40 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, jsonify, request
+from flask_cors import CORS
+from flask_mysqldb import MySQL
+import server_utils
+import json
+
+accessKey = 1234 # implementing an access key so users can not query the back end and get to decorator functions.
+
+app = Flask(__name__)  # Flask constructor
+
+CORS(app)
+
+app.config['MYSQL_HOST'] = '100.83.147.89'
+app.config['MYSQL_USER'] = 'joe'
+app.config['MYSQL_PASSWORD'] = '345573'
+app.config['MYSQL_DB'] = 'campus_navigation'
+mysql = MySQL(app)
+
+@app.route('/getLocation', methods=['POST'])
+def getLocation(): # query database for coordinates
+    cursor = mysql.connection.cursor()
+    server_utils.check_key(request.headers.get('Access-Key'), accessKey) # confirm matching access key
+    plain_text = json.dumps(request.get_json()) # convert request to plain text
+    location = plain_text[9:-1]
+    db_query = f"select coords from locations where name = {location}" # command for database
+    cursor.execute(db_query) # executing command in database
+    location_coords = cursor.fetchall() # storing results of command
+    cursor.close()
+    formatted_coords = location_coords[0][0] # returns are stored in a tuple of tuples
+    print(formatted_coords)
+    return jsonify(formatted_coords)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/test', methods=["GET", "POST"])
+def test():
+    return 'Test worked!!!'
+
+if __name__ == '__main__':
+    app.run(debug = True)
---
+++ server_utils.py
@@ -0,0 +1,5 @@
+from flask import jsonify, request
+
+def check_key(foreign_key, access_key):
+    if (foreign_key != access_key):
+        return jsonify({"error": "Unauthorized"}), 443
