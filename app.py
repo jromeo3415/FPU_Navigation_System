b4683cb8e4414aecdb2728f04f559bb455ec6326
jromeo3415 <jromeo3415@floridapoly.edu>
2025-03-26 18:31:53 -0400

    need to test calcRoute, it may need status code

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template, jsonify, request
 from flask_cors import CORS
 from flask_mysqldb import MySQL
 import server_utils
+from server_utils import check_key, getLocation, calcRoute
 import json
 
 accessKey = 1234 # implementing an access key so users can not query the back end and get to decorator functions. another access key should be received from front end.
 
+osrm_ip = "100.83.147.89"
 app = Flask(__name__)  # Flask constructor
 
 CORS(app)
@@ -16,23 +18,13 @@ app.config['MYSQL_PASSWORD'] = '345573'
 app.config['MYSQL_DB'] = 'campus_navigation'
 mysql = MySQL(app)
 
-@app.route('/getLocation', methods=['POST'])
-def getLocation(): # query database for coordinates
-    cursor = mysql.connection.cursor()
-    server_utils.check_key(request.headers.get('Access-Key'), accessKey) # confirm matching access key
-    plain_text = json.dumps(request.get_json()) # convert request to plain text
-    location = plain_text[9:-1]
-    db_query = f"select coords from locations where name = {location}" # command for database
-    cursor.execute(db_query) # executing command in database
-    location_coords = cursor.fetchall() # storing results of command
-    cursor.close()
-    formatted_coords = location_coords[0][0] # returns are stored in a tuple of tuples
-    print(formatted_coords)
-    return jsonify(formatted_coords)
+'''
+expects a JSON packet with format;  "key": "key1", "locations": "start", "destination", "profile": "foot".
+start and destination will be the start and destination's names respectively, in plain text.
+profile is the method of travel, car, foot, or bike.
+this function will turn those strings into coords then those two coords into one route, and send that route back in a JSON response
+'''
 
-#   expects a JSON packet with format;  "key": "key1", "locations": "location_1", "location_2"
-#   location_1 and location_2 will be start and destination's names respectively, in plain text
-#   this function will turn those strings into coords then those two coords into one route, and send the route back in a JSON response
 @app.route('/returnRoute', methods=['POST'])
 def returnRoute():
     request_dict = request.get_json() # converting JSON request to a Python dictionary
@@ -40,12 +32,12 @@ def returnRoute():
     if not request_dict: # if no locations were sent, send back error
         return jsonify({"error": "Request contents not received"}), 400
 
-    server_utils.check_key(request_dict["string"], accessKey) # checking if access key is valid so clients cannot stumble upon this page
+    check_key(request_dict["key"], accessKey) # checking if access key is valid so clients cannot stumble upon this page
 
-    coordinates = getLocation(request_dict["locations"]) # converting name strings into respective coordinates
+    coordinates = getLocation(mysql, request_dict["locations"]) # converting name strings into respective coordinates
 
-
-
+    route = calcRoute(osrm_ip, coordinates, request_dict["profile"])
+    return route
 @app.route('/')
 def index():
     return render_template("index.html")
--- server_utils.py
+++ server_utils.py
@@ -1,7 +1,51 @@
+import requests
 from flask import jsonify, request
 
 def check_key(foreign_key, access_key):
-    if (foreign_key != access_key):
+    if foreign_key != access_key:
         return jsonify({"error": "Unauthorized"}), 443
 
-def getLocation(dictionary):
+'''
+takes MySQL object and dictionary of location names (only two locations, maybe implement to have multiple?).
+then queries database to retrieve the coordinates in decimal degrees in a tuple.
+'''
+def getLocation(sql, dictionary):
+    start = dictionary[0] # storing start and destination strings from dictionary
+    destination = dictionary[1]
+
+    if not start or not destination: # checking if one of the locations are missing
+        return jsonify({"error": "Missing one or more locations"}), 400
+    try:
+        cursor = sql.connection.cursor() # object to represent database
+
+        db_query = f"select coords from locations where name in ('{start}', '{destination}')" # query for database
+        cursor.execute(db_query) # executing command on database
+        coords = cursor.fetchall() # retrieving results of command
+
+        coords_tuple = [coords[0][0], coords[1][0]] # converting from tuple of tuples to tuple
+        clean_tuple = (coords_tuple[0].replace(", ", ","), coords_tuple[1].replace(", ", ","))
+        #print(clean_tuple[0])
+        #print(clean_tuple[1])
+        return clean_tuple
+
+    except Exception as e:
+        err_message = str(e)
+        return jsonify({"error": f"An error occurred while fetching location coordinates: {err_message}"}), 400
+
+'''
+takes coordinates in decimal degrees in a tuple along with profile (transportation method).
+calls OSRM docker container to make a route between two points.
+returns route in JSON format.
+'''
+def calcRoute(osrm_ip, coords, profile):
+    request_url = f"{osrm_ip}/route/v1/{profile}/{coords[0]};{coords[1]}" # request to be sent to OSRM
+
+    try:
+        response = requests.get(request_url) # requests send HTTP request to OSRM. This variable holds the JSON response
+        if requests.status_codes == 200:
+            return response # front end (leaflet) prefers JSON format for route
+        else:
+            return jsonify({"error": "OSRM request error"}), 400
+
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error": f"RequestException: {str(e)}"})
